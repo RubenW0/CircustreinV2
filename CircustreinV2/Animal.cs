e3af0be143fda4749349259db1fb278542f31d54
RubenW0 <rubenwassenberg@gmail.com>
2024-09-17 14:43:10 +0200

    Add project files.

---
+++ CircustreinV2/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircustreinV2
+{
+    public class Animal
+    {
+        public enum Diet
+        {
+            Carnivore,
+            Herbivore
+        }
+
+        public enum Size
+        {
+            Small = 1,
+            Medium = 3,
+            Large = 5
+        }
+
+        private Diet AnimalDiet { get; set; }
+        private Size AnimalSize { get; set; }
+
+        public Animal(Diet diet, Size size)
+        {
+            AnimalDiet = diet;
+            AnimalSize = size;
+        }
+
+        public Diet GetDiet()
+        {
+            return AnimalDiet;
+        }
+
+        public Size GetSize()
+        {
+            return AnimalSize;
+        }
+
+    }
+}
+
---
+++ CircustreinV2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Circustrein;
+using CircustreinV2;
+
+namespace Circustrein
+{
+    internal class Program
+    {
+        public static List<Animal> animals;
+
+        static void Main()
+        {
+            animals = new List<Animal>();
+
+            animals.Add(new Animal(Animal.Diet.Carnivore, Animal.Size.Small));
+            animals.Add(new Animal(Animal.Diet.Herbivore, Animal.Size.Small));
+            animals.Add(new Animal(Animal.Diet.Carnivore, Animal.Size.Medium));
+            animals.Add(new Animal(Animal.Diet.Herbivore, Animal.Size.Medium));
+            animals.Add(new Animal(Animal.Diet.Carnivore, Animal.Size.Large));
+            animals.Add(new Animal(Animal.Diet.Herbivore, Animal.Size.Large));
+
+            PrintAnimalList();
+        }
+
+        static void PrintAnimalList()
+        { 
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"Animal Diet: {animal.GetDiet()}, Size: {animal.GetSize()}");
+            }
+        }
+    }
+}
